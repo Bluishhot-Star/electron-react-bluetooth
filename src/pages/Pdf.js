89343faab1016a41383baeee6a582fcce6af3ae0
minseok <alsthr0338@gmail.com>
2024-01-13 21:42:03 +0900

    pdf Make - 2

--- src/pages/Pdf.js
+++ src/pages/Pdf.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import PdfView from "../components/PdfView.js";
 import { PDFViewer } from '@react-pdf/renderer';
+import { useNavigate,useLocation } from 'react-router-dom'
 
 function Pdf() {
-
+  let navigatorR = useNavigate();
+  const location = useLocation();
+  const state = location.state;
+  useEffect(()=>{
+    console.log(state);
+  },[])
   return (
-    
-      <PDFViewer style={{width:1000, height:500}}>
-        <PdfView />
+      <div>
+        <div onClick={()=>{navigatorR(-1)}}>back</div>
+      <PDFViewer style={{width:1100, height:700}}>
+        <PdfView data={state}/>
       </PDFViewer> 
+      </div>
   );
 }
 
